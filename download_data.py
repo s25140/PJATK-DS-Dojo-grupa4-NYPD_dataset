92ab10f1ba79d37ebf7acf8f16821e8ce90d0ec9
ridz <ridzz.dd@gmail.com>
2024-06-08 02:52:18 +0200

    Download data thought API added

--- download_data.py
+++ download_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import requests
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -6,6 +7,13 @@ import threading
 import time
 import logging
 
+col_names = ['cmplnt_num', 'cmplnt_fr_dt', 'cmplnt_fr_tm', 'cmplnt_to_dt', 'cmplnt_to_tm', 'addr_pct_cd', 'rpt_dt',
+             'ky_cd','ofns_desc', 'pd_cd', 'pd_desc', 'crm_atpt_cptd_cd', 'law_cat_cd', 'boro_nm', 'loc_of_occur_desc',
+             'prem_typ_desc','juris_desc', 'jurisdiction_code', 'parks_nm', 'hadevelopt', 'housing_psa', 'x_coord_cd',
+             'y_coord_cd','susp_age_group', 'susp_race', 'susp_sex', 'latitude', 'longitude', 'lat_lon', 'patrol_boro',
+             'station_name','vic_age_group', 'vic_race','vic_sex', ':@computed_region_efsh_h5xi',
+             ':@computed_region_f5dn_yrer', ':@computed_region_yeji_bk3q',':@computed_region_92fq_4b7q',
+             ':@computed_region_sbqj_enih', 'transit_disrict']
 
 socrata_url = "https://data.cityofnewyork.us/resource/qgea-i56i.json"
 data_len = 8914838
@@ -13,6 +21,19 @@ batch_size = 100000
 lock = threading.Lock()
 
 logging.basicConfig(filename='errors.log', level=logging.ERROR)
+
+
+def preprocess_data(df):
+    # There were `computed_region` columns, that I suppose came from API call, so I drop them at the start
+    # Also, lat_lon - is redundant column, that I see from the beginning, will drop it too.
+    df.drop([':@computed_region_efsh_h5xi',
+             ':@computed_region_f5dn_yrer', ':@computed_region_yeji_bk3q', ':@computed_region_92fq_4b7q',
+             ':@computed_region_sbqj_enih', 'lat_lon'], inplace=True, axis=1)
+
+    # Change None valuaes to actual None, to reduce the memory(before DataSet was 3.7Gb, now 2.5Gb)
+    df.replace('(null)', None, inplace=True)
+    df.replace('UNKNOWN', None, inplace=True)
+    return df
 
 
 def fetch_data_and_save(offset, limit, max_retries=3):
@@ -29,10 +50,11 @@ def fetch_data_and_save(offset, limit, max_retries=3):
 
             # Convert to DataFrame and save to CSV
             df = pd.DataFrame(data)
+            df = preprocess_data(df)
 
             # Use lock to safely write to the file
             with lock:
-                df.to_csv('nyc_data.csv', mode='a', header=False, index=False)
+                df.to_csv('data/nyc_data.csv', mode='a', header=False, index=False)
 
             # If successful, break out of the retry loop
             break
@@ -42,13 +64,13 @@ def fetch_data_and_save(offset, limit, max_retries=3):
 
 
 def main():
-    offsets = range(0, data_len, batch_size)
+    offsets = range(1, data_len, batch_size)
 
-    # Create a CSV file and write the header (if needed)
     response = requests.get(socrata_url, params={'$limit': 1, '$offset': 0})
-    data = response.json()  # Fetch just one record to get the header
-    pd.DataFrame(data).to_csv('nyc_data.csv', mode='w', index=False)
-
+    data = response.json()
+    df = pd.DataFrame(data)
+    df['transit_district'] = [None]
+    preprocess_data(df).to_csv('data/nyc_data.csv', mode='w', index=False)
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(fetch_data_and_save, offset, batch_size, 3) for offset in offsets]
